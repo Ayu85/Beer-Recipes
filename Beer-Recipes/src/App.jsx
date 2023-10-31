b12b65eba1451d0f3b78e2f6ea82f3624d450727
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-31 15:28:37 +0530

    router dom feature added

--- Beer-Recipes/src/App.jsx
+++ Beer-Recipes/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Body from './components/Body'
+import { useState } from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import About from "./components/About";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  createBrowserRouter,
+} from "react-router-dom";
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+    ],
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+]);
 function App() {
-
-
   return (
     <>
-      <Header/>
-      <Body/>
+      <Header />
+      {/* <Body/> */}
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Beer-Recipes/src/components/Header.jsx
+++ Beer-Recipes/src/components/Header.jsx
@@ -1,18 +1,21 @@
-import logo from '../media/logo.png'
+import { Link } from "react-router-dom";
+import logo from "../media/logo.png";
 function Header() {
-    return (
-    <div className='header'>
-        <div className='logo'>
-            <img src={logo} width={150}/>
-        </div>
-        <div className='nav-buttons'>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </div>
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={logo} width={150} />
+      </div>
+      <div className="nav-buttons">
+        <ul>
+          <li>Home</li>
+          <Link to={"/about"}>
+            <li>About</li>
+          </Link>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
-    )
+  );
 }
 export default Header;
