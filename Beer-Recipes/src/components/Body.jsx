9197a800ce066a81fefec255479548a851ae2923
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-31 08:30:09 +0000

    filter and search facility done

---
+++ Beer-Recipes/src/components/Body.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
+function Body() {
+    const [searchText, setsearchText] = useState();
+    let [beerData, setbeerData] = useState([]);
+    let [filteredData, setfilteredData] = useState([]);
+    // console.log(beerData);
+    console.log(filteredData);
+    function getFdata(searchText) {
+        let d = beerData.filter((items) => {
+            return items.name.includes(searchText);
+        })
+        return d;
+    }
+    useEffect(() => {
+        getBeers()
+
+    }, [])
+    const getBeers = async () => {
+        const raw = await fetch("https://api.punkapi.com/v2/beers");
+        const jsonBeer = await raw.json();
+        setbeerData(jsonBeer);
+        setfilteredData(jsonBeer)
+    }
+    return (
+        <div>
+            <div className="search-area">
+                <input type="text" placeholder="Search your authentic taste" value={searchText} onChange={(e) => {
+                    setsearchText(e.target.value)
+                        ;
+                }} />
+                <button onClick={() => {
+                    const fd = getFdata(searchText);
+                    setfilteredData(fd);
+
+                }}>Find</button>
+            </div>
+            <div className="body">
+                {filteredData.map((items) => {
+                    return <Card {...items} />
+
+                })}
+
+            </div>
+
+        </div>
+    )
+}
+export default Body;
