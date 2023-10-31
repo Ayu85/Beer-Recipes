6396a343fc083628bc1dfe2595429efeaa4f3f5a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-31 19:32:14 +0530

    shimmer ui

--- Beer-Recipes/src/components/Card.jsx
+++ Beer-Recipes/src/components/Card.jsx
@@ -1,11 +1,11 @@
-function Card({id,name,image_url,tagline,description}) {
-    return (
-        <div className="card">
-            <img src={image_url} />
-            <h2 className="name">{name.substring(0,5)}</h2>
-            <h2 className="tag">{tagline}</h2>
-            <h3>{description.substring(0,150)}</h3>
-        </div>
-    )
+function Card({ id, name, image_url, tagline, description }) {
+  return (
+    <div className="card">
+      <img src={image_url} />
+      <h2 className="name">{name}</h2>
+      <h2 className="tag">{tagline}</h2>
+      <h3>{description}</h3>
+    </div>
+  );
 }
 export default Card;
--- Beer-Recipes/src/components/Shimmer.jsx
+++ Beer-Recipes/src/components/Shimmer.jsx
@@ -1,2 +1,17 @@
-
-
+function ShimmerCard() {
+  return (
+    <div className="shimmer-card">
+      <div className="shimmer-box1"></div>
+      <div className="shimmer-box2"></div>
+      <div className="shimmer-box3"></div>
+    </div>
+  );
+}
+function Shimmer() {
+  return (
+    <div className="shimmer-body">
+      <ShimmerCard />
+    </div>
+  );
+}
+export default Shimmer;
